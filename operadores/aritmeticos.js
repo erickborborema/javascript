38e00fa9f4d6d6bb1b376254b970edeafbf15a9a
erickborborema <erick.silva.borborema7@gmail.com>
2025-01-29 11:03:48 -0300

    Lista

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,29 @@
+//console.log("Olá, Mundo!")
+
+//para declarar variáveis
+let nome = "Juliana" //vamos preferir esse
+var nomes = "Juliana"
+
+//CTRL + ; -> comenários
+console.log(nome)
+
+let idade = 24
+console.log("Minha idade é ", idade, "anos")
+//CONCATENÇÃO = juntar tetxo e valor
+
+//OPERAÇÕES MATEMÁTICAS
+let a = 3
+let b = 2
+
+let soma = a + b 
+let sub = a - b 
+let mult = a * b
+let div = a / b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+console.log("oi")
---
+++ operadores/relacionais.js
@@ -0,0 +1,10 @@
+/* > maior
+>=maior ou igual
+<menor
+<menor ou igual
+==igual
+!=diferente*/
+
+let idade = 18
+let resultado = idade > 18
+console.log(resultado)
